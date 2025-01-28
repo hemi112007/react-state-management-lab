98b9e1e0f2d8592e90085cc58c6bc617b1d5283f
hamdan.ga <hamdanchitari@gmail.com>
2025-01-28 22:48:06 +0300

    reset not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+//App.jsx
+
 // src/App.jsx
 import { useState } from "react";
 import './App.css';
@@ -94,7 +96,8 @@ const initialState = [
   const [team, setTeam] = useState([])
   const [money, setMoney] = useState(100)
   const [zombieFighters, setZombieFighters] = useState(initialState)
-
+  const totalStrength = team.reduce((sum, fighter) => sum + fighter.strength, 0);
+  const totalAgility = team.reduce((sum, fighter) => sum + fighter.agility, 0);
   function handleAddFighter (fighter) {
 
     if (money < fighter.price){
@@ -104,26 +107,44 @@ const initialState = [
     //make a copy
     //modify the copy if required
     const newTeam = [...team, fighter]
-
-    //update the state
-    setTeam (newTeam)
-
-    
     const newZombieFighters = zombieFighters.filter(zombieFighter => (
       zombieFighter.id !== fighter.id
     ))
-
+    //update the state
+    setTeam (newTeam, fighter);
     setZombieFighters(newZombieFighters)
-
     const newMoney = money - fighter.price
     setMoney (newMoney)
-
+    const TotalStrength = 0 
+    
   }
+  function handleRemoveFighter(fighter) {
+    setTeam(team.filter((member) => member.id !== fighter.id));
+    setZombieFighters([...zombieFighters, fighter]);
+    setMoney(money + fighter.price);
+  }
+  function resetGame (){}
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1>Hello Boss </h1>
       <h3>Money: {money}</h3>
+      <h3>Team Strenght: {totalStrength}</h3>
+      <h3>Agility: {totalAgility}</h3>
+      <h3>My Team</h3>
+      <ul>
+        {team.map(fighter => (
+          <li>
+            <img src={fighter.img} alt={fighter.name} /> 
+            <h3>{fighter.name}</h3>
+            <p>price: {fighter.price}</p>
+            <p>strength: {fighter.strength}</p>
+            <p>agility: {fighter.agility}</p>
+            <button onClick={() => handleRemoveFighter (fighter)} >Remove</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Fighters</h2>
       <ul>
         {zombieFighters.map(fighter => (
           <li>
@@ -133,11 +154,13 @@ const initialState = [
             <p>strength: {fighter.strength}</p>
             <p>agility: {fighter.agility}</p>
             <button onClick={() => handleAddFighter (fighter)} >Add</button>
-            </li>
+          </li>
         ))}
       </ul>
+      <button onClick={resetGame} style={{ marginTop: "20px" }}>
+        Reset Game
+      </button>
     </>
   );
 }
-
 export default App
